708572484446ed1f2414eae7d9d69202e6bcfe96
ArailymMedetbek <78314206+ArailymMedetbek@users.noreply.github.com>
2021-03-19 13:56:41 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,41 +2,40 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
-app.set("port", 3000 || process.env.PORT); //ЗАДАЕМ ПОРТ ИЛИ ТОТ КОТОРЫЙ ФРИ
+app.set("port", 80 || process.env.PORT); //ЗАДАЕМ ПОРТ ИЛИ ТОТ КОТОРЫЙ ФРИ
 
 app.use(express.static(path.join(__dirname, "/"))); //дает доступ чтобы открылись все другие файлы
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.status(200);
     res.contentType("text/html");
     res.sendFile(path.join(__dirname, "/index.html"))
 });
 
-app.get("/about", (req, res) =>{
+app.get("/about", (req, res) => {
     res.status(200);
     res.contentType("text/html");
     res.sendFile(path.join(__dirname, "/about.html"))
 });
 
-app.get("/about/team_photo", (req, res) =>{
+app.get("/about/team_photo", (req, res) => {
     res.status(200);
     res.contentType("image/jpg");
     res.sendFile(path.join(__dirname, "/teamphoto.jpg"))
 });
 
-app.get("/video", (req, res) =>{
+app.get("/video", (req, res) => {
     res.status(200);
     res.contentType("video/mp4");
     res.sendFile(path.join(__dirname, "/video.mp4"))
 });
 
-app.get("err", (req, res) =>{
+app.get("err", (req, res) => {
     res.status(200);
     res.contentType("text/plain");
     res.sendFile(404, NOTFOUND)
 });
 
-
-app.listen(app.get("port"), ()=>{
+app.listen(app.get("port"), () => {
     console.log(`Express server is running on localhost:${app.get("port")}. Please CTRL+C to terminate...`)
 })
